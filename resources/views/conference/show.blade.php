686a1ddcd5ffe988ea0fc6bc2c38c35626488d77
Mantas <mantas.ambrazaitis9@gmail.com>
2023-04-21 23:00:39 +0300

    feat: create conference form

--- resources/views/conference/show.blade.php
+++ resources/views/conference/show.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+    @if(session('status'))
+        <div style="background-color: green; color: lime">{{ session('status') }}</div>
+    @endif
     <h2>#{{ $conference['id'] }}</h2>
     <h2>Title</h2>
     <p>{{ $conference['title'] }}</p>
