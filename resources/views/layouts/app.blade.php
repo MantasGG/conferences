ccc56f89dcef3e272f51c8050ca4017568b12bc3
Mantas <mantas.ambrazaitis9@gmail.com>
2023-04-22 14:28:38 +0300

    feat: authorization functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,27 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>Laravel project - @yield('title')</title>
 </head>
-<body>
+<body style="margin: 20px;">
+<div>
+    <header class="m-10">
+        @guest
+            <a href="{{ route('login') }}">Login</a>
+        @else
+            <a href="{{ route('logout') }}"
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                Logout ({{ auth()->user()->name }})
+            </a>
+            <form action="{{ route('logout') }}" method="POST" id="logout-form" class="d-none">
+                @csrf
+            </form>
+        @endguest
+    </header>
+</div>
     @yield('content')
+
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
